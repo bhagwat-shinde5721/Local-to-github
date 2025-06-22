9f81422240552410f505c65e342e42ff04cd61ea
bhagwat-shinde5721 <bhagwat.shinde19@vit.edu>
2025-06-22 15:01:15 +0530

    First change in python file

---
+++ FirstPython.py
@@ -0,0 +1,16 @@
+rows = 6  # You can change the number of rows
+
+for i in range(1, rows + 1):
+    # Print leading spaces
+    print(" " * (rows - i), end="")
+
+    # Print ascending numbers
+    for j in range(1, i + 1):
+        print(j, end="")
+        print("//????")
+
+    # Print descending numbers
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+
+    print()  # Move to next line
