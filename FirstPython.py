9700df6d9aef6ab72f9033ed25ba3728df0de3f3
bhagwat-shinde5721 <bhagwat.shinde19@vit.edu>
2025-06-22 21:25:30 +0530

    Changing output statement

--- FirstPython.py
+++ FirstPython.py
@@ -11,6 +11,6 @@ for i in range(1, rows + 1):
 
     # Print descending numbers
     for j in range(i - 1, 0, -1):
-        print(j, end="")
+        print(j, end="Bhagwat Shinde")
 
     print()  # Move to next line
