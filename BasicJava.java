755e57ac6bd1216605e72dcafe7e06ff06d01460
bhagwat-shinde5721 <bhagwat.shinde19@vit.edu>
2025-06-22 16:18:54 +0530

    Create BasicJava.java

---
+++ BasicJava.java
@@ -0,0 +1,10 @@
+public class BasicJava{
+  public void show(Sring name){
+    System.out.println("Hi, " + name)
+      }
+  public static void main(String[] args)
+  {
+    BasicJava b = new BasicJava();
+    b.show();
+  }
+}
