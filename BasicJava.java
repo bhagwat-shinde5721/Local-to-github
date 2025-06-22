8ef8c3fa39b475959f0c86d778d47cedf1b52ecb
bhagwat-shinde5721 <bhagwat.shinde19@vit.edu>
2025-06-22 23:07:00 +0530

    Updating java File

--- BasicJava.java
+++ BasicJava.java
@@ -2,14 +2,19 @@ public class BasicJava{
   public void show(String name){
     System.out.println("Hi, " + name);
       }
-    public String Basic()
+    public String Basic(int a)
     {
+      for(int i = 1 ; i<= a ; i++)
+      {
+        System.out.print(" "+i);
+      }
       return "Heloooooo!!!!";
     }
   public static void main(String[] args)
   {
     BasicJava b = new BasicJava();
     b.show("Bhagwat Shinde");
-    System.out.println(b.Basic());
+    System.out.println(b.Basic(20));
+
   }
 }
