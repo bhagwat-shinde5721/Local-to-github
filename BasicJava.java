936a10f917c7852e996816a13f2fece49fff3774
bhagwat-shinde5721 <bhagwat.shinde19@vit.edu>
2025-06-22 22:22:42 +0530

    Pushing Changes to Java File

--- BasicJava.java
+++ BasicJava.java
@@ -2,9 +2,14 @@ public class BasicJava{
   public void show(String name){
     System.out.println("Hi, " + name);
       }
+    public String Basic()
+    {
+      return "Heloooooo!!!!";
+    }
   public static void main(String[] args)
   {
     BasicJava b = new BasicJava();
     b.show("Bhagwat Shinde");
+    System.out.println(b.Basic());
   }
 }
